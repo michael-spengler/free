dd026bf7eb41156dbe1d71768c786a4703cd1867
Michael <michael.spengler@sap.com>
2023-09-01 14:20:06 +0200

    add some state changing interaction options

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,7 +2,7 @@
 	import Metamask from "./Metamask.svelte";
 	import { ethers } from "ethers";
 	import { smartContractABI } from './constants.ts';
-	
+
 	export let projectTitle;
 	export let targetChainId;
 	export let targetChainName;
@@ -11,16 +11,30 @@
 	let publicWalletAddressOfVisitor;
 	let connectedToChainId;
 	let amountOfCoinsInSmartContractItself;
+	let amountOfCoinsInVisitorsWallet;
+	let contract;
 	
 	async function handleWalletConnected(event) {
 		publicWalletAddressOfVisitor = event.detail.publicWalletAddress;
 		connectedToChainId = event.detail.chainId;
 		const provider = new ethers.BrowserProvider(window.ethereum)
-		//const signer = await provider.getSigner()
-		let contract = new ethers.Contract(smartContractAddressOnChain, smartContractABI, provider)
+		const signer = await provider.getSigner()
+		contract = new ethers.Contract(smartContractAddressOnChain, smartContractABI, signer)
 		amountOfCoinsInSmartContractItself = 
 		ethers.formatEther((await contract.balanceOf(smartContractAddressOnChain)));
+		amountOfCoinsInVisitorsWallet = 
+		ethers.formatEther((await contract.balanceOf(publicWalletAddressOfVisitor)));
 	}
+
+	async function startProject() {
+		await contract.startProject()
+		window.location.reload();
+	}
+	async function claimCurrentlyAvailableLiquidityBackedMaxRewards() {
+		await contract.claimCurrentlyAvailableLiquidityBackedMaxRewards()
+		confirm("please reload this page after some seconds to see how much coins you have")
+	}
+
 </script>
 
 <main>
@@ -52,12 +66,25 @@
 		<p><br></p>
 
 		This dApp allows you to interact with the following smart contract: <p></p>
-		{smartContractAddressOnChain} on {targetChainName}.
+	 	<a target="_blank" href="https://zkevm.polygonscan.com/address/{smartContractAddressOnChain}">
+			{smartContractAddressOnChain}
+		</a>	
+		 on {targetChainName}.
 
 		<p><br></p>
-		Within the smart contract itself there are currently <p><br></p>
+		Within the smart contract itself there are currently <p></p>
 		{amountOfCoinsInSmartContractItself} Coins.
-
+		<p><br></p>
+		You currently have <p></p>
+		{amountOfCoinsInVisitorsWallet} Coins.
+		<p><br></p>
+		<button on:click={startProject}>
+			Start Project
+		</button>
+		<p><br></p>
+		<button on:click={claimCurrentlyAvailableLiquidityBackedMaxRewards}>
+			Claim Currently Available Rewards
+		</button>
 	{:else}
 		Please check Metamask Browserextension and reload.
 	{/if}
